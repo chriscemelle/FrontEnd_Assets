2f3f9eb6207fcb8a0504468ec8a8fe7f79e14adc
Chriscemelle <chriscabudlan@gmail.com>
2021-07-23 18:49:03 +0800

    first commit

---
+++ components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <b-navbar toggleable="sm" type="light" variant="info">
+
+            <b-navbar-brand href="/">Asset</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                
+                <b-navbar-nav class="ml-auto">
+
+                    <template v-if="$auth.loggedIn">
+                        <b-nav-item href="/dashboard">Dashboard</b-nav-item>
+                        <b-nav-item href="/assets">Asset</b-nav-item>
+                        <b-nav-item href="#" @click.prevent="onLogout">Logout</b-nav-item>
+                    </template>
+
+                    <template v-else>
+                        <b-nav-item href="/login">Login</b-nav-item>
+                    </template>
+
+                </b-navbar-nav>
+
+            </b-collapse>
+
+        </b-navbar>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        async onLogout() {
+            this.$auth.logout()
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NavBar />
+    
+    <div class="container">
+      <h1>Welcome</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false
+}
+</script>
